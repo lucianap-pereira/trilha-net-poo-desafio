7be2b4a65ad08eae8f5fe7478137bcd6afa75d9b
Luciana Pereira <lucianasp_04@hotmail.com>
2024-05-21 10:17:04 -0300

    Desafio Conclído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Smartphone Nokia:");
+            Smartphone nokia = new Nokia(numero: "789456", modelo: "Modelo 1", imei: "011", memoria: 64);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Whatsapp");
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Smartphone iPhone:");
+            Smartphone iphone = new Iphone(numero: "6549", modelo: "Modelo 2", imei: "022", memoria: 128);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Telegram");
+        }
+    }
+}
